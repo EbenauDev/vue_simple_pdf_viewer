46f7165a189ee46aafe2ca8399817c12dda440c7
Joao Tiago <ebenau06@gmail.com>
2020-06-30 20:16:46 -0300

    Adicionado lib vue-pdf

--- src/components/PDFViewer.vue
+++ src/components/PDFViewer.vue
@@ -1,29 +1,62 @@
 <template>
   <div>
-    <button v-searchPDF.funcoes="{callback: resultSearchPDF}">Search PDF</button>
+    <label class="button" v-searchPDF="{callback: resultSearchPDF}">
+      <input type="file" hidden accept="application/pdf" />
+      Search PDF
+    </label>
+    <div v-if="arrayDeBits">
+      <pdf :src="arrayDeBits"></pdf>
+    </div>
   </div>
 </template>
 
 <script>
+import pdf from "vue-pdf";
 export default {
   name: "PDFViewer",
+  components: {
+    pdf
+  },
   directives: {
     searchPDF: function(el, binding) {
-      console.log(el);
-      console.log(binding);
-      binding.value.callback("teste");
+      const input = el.querySelector("input");
+
+      const fileReader = new FileReader();
+      input.addEventListener("change", handleFile, false);
+
+      function handleFile({ target }) {
+        fileReader.readAsArrayBuffer(target.files[0]);
+        fileReader.onload = () => {
+          getResultReadFile(fileReader.result);
+        };
+      }
+      function getResultReadFile(file) {
+        binding.value.callback && binding.value.callback(file);
+      }
     }
   },
   methods: {
     resultSearchPDF(response) {
-      console.log(response);
+      this.arrayDeBits = new Uint8Array(response);
     }
+  },
+  data() {
+    return {
+      arrayDeBits: null
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-button {
+.button {
   padding: 10px 20px;
+  cursor: pointer;
+  border: 0;
+  border-radius: 4px;
+  background-color: #ffffff;
+  &:active {
+    filter: contrast(0.8);
+  }
 }
 </style>
