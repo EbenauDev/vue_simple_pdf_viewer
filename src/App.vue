c986baa59f305a414d308b0460e9fdd99e1789a5
Joao Tiago <ebenau06@gmail.com>
2020-06-30 18:58:38 -0300

    Adicionado sass

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PDFViewer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PDFViewer from "./components/PDFViewer";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    PDFViewer
   }
-}
+};
 </script>
 
 <style>
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #1d3754;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/PDFViewer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button v-searchPDF.funcoes="{callback: resultSearchPDF}">Search PDF</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PDFViewer",
+  directives: {
+    searchPDF: function(el, binding) {
+      console.log(el);
+      console.log(binding);
+      binding.value.callback("teste");
+    }
+  },
+  methods: {
+    resultSearchPDF(response) {
+      console.log(response);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 10px 20px;
+}
+</style>
